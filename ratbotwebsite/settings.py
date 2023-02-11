1c87de531a60b7d0413e53be7451de82927bbe3e
negrahodzic <negra.hodzic@gmail.com>
2023-02-11 07:31:02 +0100

    Static files fix 4 - DEBUG True

--- ratbotwebsite/settings.py
+++ ratbotwebsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%-9c(thmw8+-eez=l)z9hp3#_njdhj#s$fm)o4gnzfa14xykbg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
