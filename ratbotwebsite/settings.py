e041b75c5eb32c21b38d02ada1033244162f5123
negrahodzic <negra.hodzic@gmail.com>
2023-02-11 21:33:34 +0100

    CSRF CORS fix 9 - ChatGPT says it sees the problem. CSRF_TRUSTED_ORIGINS  and CORS_ORIGIN_ALLOW_ALL are conflicting

--- ratbotwebsite/settings.py
+++ ratbotwebsite/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 # CORS_ORIGIN_WHITELIST = [
 #     'https://rat-bot.up.railway.app',
 # ]
