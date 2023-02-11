cea259bdf7ee706368fc05bb1c884a9783295925
negrahodzic <negra.hodzic@gmail.com>
2023-02-11 20:48:38 +0100

    CSRF CORS fix 7 - CORS_ORIGIN_ALLOW_ALL

--- ratbotwebsite/settings.py
+++ ratbotwebsite/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = [
 #     'https://rat-bot.up.railway.app',
 # ]
